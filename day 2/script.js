1f1f2a1bde28f2d24a45b99ddf41b0b28f9bcd20
Yusufsaheed22 <107994014+Yusufsaheed22@users.noreply.github.com>
2022-08-24 22:43:22 +0100

    Update script.js

--- day 2/script.js
+++ day 2/script.js
@@ -1,2 +1,101 @@
-let e = "sweet";
-console.log(e);
+let challenge = '30 Days Of javaScript'
+console.log (challenge)
+let js = '30 Days Of javaScript'
+console.log(js.length)
+let word = '30 Days Of javaScript'
+console.log(word.toUpperCase('30 Days Of javaScript'))
+console.log(word.toLowerCase('30 Days Of javaScript'))
+ let string = '30 Days Of javaScript'
+ console.log(3,20)
+let phrase = '30 Days Of javaScript'
+console.log(phrase.substr(3,18))
+let words = '30 Days Of javaScript'
+console.log(phrase.includes('Script'))
+let strin = '30 Days Of javaScript'
+console.log(strin.split( ))
+let stron = '30 Days Of javaScript'
+console.log(stron.split(' '))
+let apps= 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+console.log(apps.split(','))
+let m = '30 Days Of javaScript'
+console.log(m.replace('javaScript', 'python'))
+let s = '30 Days Of javaScript'
+console.log(s.charAt(15))
+let n = '30 Days Of javaScript'
+console.log(n.charCodeAt(11))
+let j = '30 Days Of javaScript'
+console.log(j.indexOf('a'))
+let q = '30 Days Of javaScript'
+console.log(q.lastIndexOf('a'))
+let g = 'You cannot end a sentence with because because because is a conjunction'
+console.log(g.indexOf('because'))
+let v = 'You cannot end a sentence with because because because is a conjunction'
+console.log(v.lastIndexOf('because'))
+let c = 'You cannot end a sentence with because because because is a conjunction'
+console.log(c.search('because'))
+let d = '30 Days Of javaScript'
+console.log(d.trim(' '))
+let h = '30 Days Of javaScript'
+console.log(h.startsWith('30 Days'))
+let p = '30 Days Of javaScript'
+console.log(p.endsWith('javaScript'))
+let f = '30 Days Of javaScript'
+let patternOne = /a/
+let z = '30 Days of'
+console.log(z.concat('JavaScript'))
+let t = '30 Days Of javaScript'
+console.log(t.repeat(2))
+const language = 'There is no exercise better for the heart than reaching down and lifting people up.'  + 'by John Holmes teaches us to help one another.'
+console.log(language)
+const sentence = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log(sentence)
+let b = '10'
+console.log(10)
+let num = '9.81'
+let numFloat = parseFloat(num)
+console.log(numFloat)
+let check = 'python'
+console.log(check.endsWith('on'))
+let checks = 'jargon'
+console.log(checks.endsWith('on'))
+let checksc = 'I hope this course is not full of jargon'
+console.log(checksc.endsWith('jargon'))
+const numm = Math.floor(Math.random () * 100) // creates random number between 0 and 100
+console.log(numm)
+//Absolute value
+console.log(Math.abs(-100))
+let randomNum = Math.random(50)         // generates 50 to 100
+let numBtnFiftyAndHundred = randomNum * 100
+console.log(numBtnFiftyAndHundred)         // this gives: min 50 and max 99.99
+const numms = Math.floor(Math.random () * 255) // creates random number between 0 and 100
+console.log(numms)
+//Absolute value
+console.log(Math.abs(-255))
+let strings = 'JavaScript'
+let firstLetter = strings[0]
+console.log(firstLetter)
+
+let secondLetter = string[1]       // a
+let thirdLetter = string[2]
+let lastLetter = string[9]
+
+console.log(lastLetter)            // t
+
+let lastIndex = string.length - 1
+
+console.log(lastIndex)  // 9
+console.log(string[lastIndex])    // t
+let stringk = 'You cannot end a sentence with because because because is a conjunction'
+console.log(stringk.substr(31,23))
+let k = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(k.search('love'))
+let stringl = 'You cannot end a sentence with because because because is a conjunction'
+console.log(stringl.match('because'))
+const txt = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(txt.match(/\d+/g))
+const txts = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month'
+console.log(txt.match(/\d+/g))
+
+
+
+
